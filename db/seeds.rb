f5f981570ed27fb03606103530c26b81c611937f
Manuel DESTOUESSE <manuel.destouesse@itrachealth.com>
2020-11-07 07:00:44 -0500

    upgrade api and swagger file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,16 @@
 SensorReading.delete_all
 Sensor.delete_all
 Device.delete_all
+SensorType.delete_all
 
 type_temp = SensorType.create(:name => 'Temperature', :unit => 'C', :pow10multi => -2)
 type_pres = SensorType.create(:name => 'Pressure', :unit => 'Pa', :pow10multi => -2)
 type_batt = SensorType.create(:name => 'Battery Level', :unit => '%', :pow10multi => 0)
+type_humidity = SensorType.create(:name => 'Humidity Level', :unit => '%', :pow10multi => -2)
 
-device = Device.create(:uuid => "123-123-000-000", :name => "Test Device", :description => "Test Device ")
+
+device = Device.create(:uuid => "123-123-000-000", :name => "MKR1010 BME280", :description => "Test Device ")
 sensor_temp = Sensor.create(:uuid => "123-123-000-001", :name => "Temperature Sensor", :description => "Temperature Sensor", :device => device, :sensor_type => type_temp)
 sensor_pres = Sensor.create(:uuid => "123-123-000-002", :name => "Pressure Sensor", :description => "Pressure Sensor", :device => device, :sensor_type => type_pres)
-sensor_batt = Sensor.create(:uuid => "123-123-000-003", :name => "Battery Level Sensor", :description => "Battery Level Sensor", :device => device, :sensor_type => type_batt)
+sensor_humidity = Sensor.create(:uuid => "123-123-000-003", :name => "Humidity Level Sensor", :description => "Battery Level Sensor", :device => device, :sensor_type => type_humidity)
+sensor_batt = Sensor.create(:uuid => "123-123-000-004", :name => "Battery Level Sensor", :description => "Battery Level Sensor", :device => device, :sensor_type => type_batt)
